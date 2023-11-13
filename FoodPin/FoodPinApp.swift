ae8c1654c232430a005cbdfd1960d3151256ee5f
m430 <360475097@qq.com>
2023-11-13 13:29:26 +0800

    Initial Commit

---
+++ FoodPin/FoodPinApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodPinApp.swift
+//  FoodPin
+//
+//  Created by 郑敏 on 2023/11/13.
+//
+
+import SwiftUI
+
+@main
+struct FoodPinApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
