60b550bdf69881515c983216b173b6677b3a6bc8
m430 <360475097@qq.com>
2023-11-14 21:17:36 +0800

    new restaurant view

---
+++ FoodPin/View/NewRestaurantView.swift
@@ -0,0 +1,20 @@
+//
+//  NewRestaurantView.swift
+//  FoodPin
+//
+//  Created by 郑敏 on 2023/11/14.
+//
+
+import SwiftUI
+
+struct NewRestaurantView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct NewRestaurantView_Previews: PreviewProvider {
+    static var previews: some View {
+        NewRestaurantView()
+    }
+}
